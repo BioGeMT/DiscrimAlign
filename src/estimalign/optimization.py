311b524a9b9d12930469958adba9d4abf8bc4875
zacharopoulou <zacharopoulou.eli@gmail.com>
2026-05-05 12:42:44 +0200

    Suppress sklearn logistic regression warnings

--- src/estimalign/optimization.py
+++ src/estimalign/optimization.py
@@ -1,6 +1,25 @@
+import warnings
+
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from Bio.Align import substitution_matrices
+
+
+def _fit_logistic_regression(logit, predictors, labels):
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message=".*penalty.*deprecated.*",
+            category=FutureWarning,
+        )
+        warnings.filterwarnings(
+            "ignore",
+            message=".*Setting penalty=None.*",
+            category=UserWarning,
+        )
+        logit.fit(predictors, labels)
+    return logit
+
 
 ### Starting point
 def _initial_estim_affinegap_simplesubs(alignment_list, labels):
@@ -17,17 +36,22 @@ def _initial_estim_affinegap_simplesubs(alignment_list, labels):
                 counts.identities,
                 counts.mismatches,
                 counts.open_gaps,
-                counts.extend_gaps
+                counts.extend_gaps,
             ]
-                )
+        )
+
     logit = LogisticRegression(fit_intercept=True, penalty=None)
-    logit.fit(predictors, labels)
-    estimates = {'alpha': logit.intercept_[0],
-                 'match_score': logit.coef_[0][0],
-                 'mismatch_score': logit.coef_[0][1],
-                 'open_gap_score': logit.coef_[0][2],
-                 'extend_gap_score': logit.coef_[0][3]}
+    _fit_logistic_regression(logit, predictors, labels)
+
+    estimates = {
+        "alpha": logit.intercept_[0],
+        "match_score": logit.coef_[0][0],
+        "mismatch_score": logit.coef_[0][1],
+        "open_gap_score": logit.coef_[0][2],
+        "extend_gap_score": logit.coef_[0][3],
+    }
     return estimates
+
 
 def _initial_estim_lineargap_simplesubs(alignment_list, labels):
     """
@@ -42,101 +66,156 @@ def _initial_estim_lineargap_simplesubs(alignment_list, labels):
             [
                 counts.identities,
                 counts.mismatches,
-                counts.gaps
+                counts.gaps,
             ]
-                )
+        )
+
     logit = LogisticRegression(fit_intercept=True, penalty=None)
-    logit.fit(predictors, labels)
-    estimates = {'alpha': logit.intercept_[0],
-                 'match_score': logit.coef_[0][0],
-                 'mismatch_score': logit.coef_[0][1],
-                 'gap_score': logit.coef_[0][2]}
+    _fit_logistic_regression(logit, predictors, labels)
+
+    estimates = {
+        "alpha": logit.intercept_[0],
+        "match_score": logit.coef_[0][0],
+        "mismatch_score": logit.coef_[0][1],
+        "gap_score": logit.coef_[0][2],
+    }
     return estimates
 
-def _initial_estim_affinegap_fullsubs(alignment_list, labels,
-                                      alphabet):
+
+def _initial_estim_affinegap_fullsubs(
+    alignment_list,
+    labels,
+    alphabet,
+):
     """
     Returns an initial estimator of alignment parameters
     using a Ridge logistic model with intercept and
-    a full set of predictors: numbers of gap extends and a substitution matrix
+    a full set of predictors: numbers of gap extends and a substitution matrix.
     """
     Asize = len(alphabet)
     predictors = []
-    pair_to_id = {(char1, char2): Asize*i + j for i, char1 in enumerate(alphabet) for j, char2 in enumerate(alphabet)}
+    pair_to_id = {
+        (char1, char2): Asize * i + j
+        for i, char1 in enumerate(alphabet)
+        for j, char2 in enumerate(alphabet)
+    }
+
     for aln in alignment_list:
         counts = aln.counts()
-        gaps = [counts.open_gaps,
-                counts.extend_gaps]
-        substitutions = [0]*(Asize**2)
+        gaps = [
+            counts.open_gaps,
+            counts.extend_gaps,
+        ]
+        substitutions = [0] * (Asize**2)
+
         for char1, char2 in zip(aln[0], aln[1]):
-            if char1 != '-' and char2 != '-':
+            if char1 != "-" and char2 != "-":
                 substitutions[pair_to_id[(char1, char2)]] += 1
-        predictors.append(substitutions+gaps)
-    
-    logit = LogisticRegression(fit_intercept=True, solver='newton-cg')
-    logit.fit(predictors, labels)
-    substitution_matrix = substitution_matrices.Array(data=np.zeros((Asize, Asize)),
-                                                      alphabet=alphabet)
+
+        predictors.append(substitutions + gaps)
+
+    logit = LogisticRegression(fit_intercept=True, solver="newton-cg")
+    _fit_logistic_regression(logit, predictors, labels)
+
+    substitution_matrix = substitution_matrices.Array(
+        data=np.zeros((Asize, Asize)),
+        alphabet=alphabet,
+    )
     for char1 in alphabet:
         for char2 in alphabet:
-            substitution_matrix[char1, char2] = logit.coef_[0][pair_to_id[(char1, char2)]]
+            substitution_matrix[char1, char2] = logit.coef_[0][
+                pair_to_id[(char1, char2)]
+            ]
+
     open_gap_score = logit.coef_[0][-2]
     extend_gap_score = logit.coef_[0][-1]
-    estimates = {'alpha': logit.intercept_[0],
-                 'substitution_matrix': substitution_matrix,
-                 'open_gap_score': open_gap_score,
-                 'extend_gap_score': extend_gap_score}
+
+    estimates = {
+        "alpha": logit.intercept_[0],
+        "substitution_matrix": substitution_matrix,
+        "open_gap_score": open_gap_score,
+        "extend_gap_score": extend_gap_score,
+    }
     return estimates
 
 
-def get_initial_estimate(alignment_list, labels,
-                         substitution_mode = 'simple',
-                         gap_mode = 'affine',
-                         alphabet=None):
+def get_initial_estimate(
+    alignment_list,
+    labels,
+    substitution_mode="simple",
+    gap_mode="affine",
+    alphabet=None,
+):
     """
     Returns an initial estimator of alignment parameters
     using a simple logistic models with intercept and
     summary predictors: numbers of matches, mismatches, and gaps.
     """
-    assert gap_mode in {'affine', 'linear'}, 'Only linear and affine gap modes are supported'
-    assert substitution_mode in {'simple', 'symmetric', 'general'}
-    if substitution_mode != 'simple':
-        assert alphabet is not None, 'General and symmetric substitution mode require to specify the alphabet'
-    if substitution_mode == 'simple':
-        if gap_mode == 'affine':
-            estimates = _initial_estim_affinegap_simplesubs(alignment_list, labels)
-        elif gap_mode == 'linear':
-            estimates = _initial_estim_lineargap_simplesubs(alignment_list, labels)
+    assert gap_mode in {
+        "affine",
+        "linear",
+    }, "Only linear and affine gap modes are supported"
+    assert substitution_mode in {"simple", "symmetric", "general"}
+
+    if substitution_mode != "simple":
+        assert (
+            alphabet is not None
+        ), "General and symmetric substitution mode require to specify the alphabet"
+
+    if substitution_mode == "simple":
+        if gap_mode == "affine":
+            estimates = _initial_estim_affinegap_simplesubs(
+                alignment_list,
+                labels,
+            )
+        elif gap_mode == "linear":
+            estimates = _initial_estim_lineargap_simplesubs(
+                alignment_list,
+                labels,
+            )
     else:
-        estimates = _initial_estim_affinegap_fullsubs(alignment_list, labels, alphabet)
-        if substitution_mode == 'symmetric':
+        estimates = _initial_estim_affinegap_fullsubs(
+            alignment_list,
+            labels,
+            alphabet,
+        )
+        if substitution_mode == "symmetric":
             # estimation of symmetric matrix should be implemented in
             # a separate function, for now we use this trick
-            subsM = estimates['substitution_matrix']
-            subsM = (subsM.T + subsM)/2
-            estimates['substitution_matrix'] = subsM
-        if gap_mode == 'linear':
-            estimates['gap_score'] = estimates['open_gap_score']+ estimates['extend_gap_score']
-            del estimates['open_gap_score']
-            del estimates['extend_gap_score']
+            subsM = estimates["substitution_matrix"]
+            subsM = (subsM.T + subsM) / 2
+            estimates["substitution_matrix"] = subsM
+
+        if gap_mode == "linear":
+            estimates["gap_score"] = (
+                estimates["open_gap_score"] + estimates["extend_gap_score"]
+            )
+            del estimates["open_gap_score"]
+            del estimates["extend_gap_score"]
+
     return estimates
+
 
 ### Parallel processing
 def create_alignment_workers(seqlistA, seqlistB, aligner):
     """
-    Create joblib parallel workers
+    Create joblib parallel workers.
     """
     from joblib import delayed
+
     def return_alignment(seqA, seqB, aligner):
         aln = aligner.align(seqA, seqB)
         return next(aln)
+
     for seqA, seqB in zip(seqlistA, seqlistB):
         yield delayed(return_alignment)(seqA, seqB, aligner)
-        
+
+
 ### Subgradient method stepfunctions
 def create_constant_step(scale):
     def step(niter):
         return scale
+
     return step
 
 
@@ -146,11 +225,13 @@ def create_powerstep(scale, power=0.5, burnin=0):
     scale is equal to scale/iteration_number**power.
     Typically power == 0.5.
     The power scaling kicks in after a burnin number of steps, before
-    which it's equal to scale. 
+    which it's equal to scale.
     """
+
     def step(niter):
         if niter >= burnin:
-            return scale/(niter - burnin + 1)**power
+            return scale / (niter - burnin + 1) ** power
         else:
             return scale
+
     return step
